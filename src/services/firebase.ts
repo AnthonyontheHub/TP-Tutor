0f7c004ebd42d19d78f46f0ba55e2d4d79f86d69
AnthonyontheHub <Anthony.Guera@gmail.com>
2026-04-20 20:50:41 -0500

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_FROM_FIREBASE_CONSOLE", // You must paste your real API key here
-  authDomain: "tp-tutor-26792.firebaseapp.com",
-  projectId: "tp-tutor-26792",
-  storageBucket: "tp-tutor-26792.appspot.com",
-  messagingSenderId: "331006450625", // Pulled from your Project Number
-  appId: "YOUR_APP_ID_FROM_FIREBASE_CONSOLE" // You must paste your real App ID here
+  apiKey: "AIzaSyBCK-D_zNi8LazyaEhP9VLUYWeeGa2QXcI", // You must paste your real API key here
+  authDomain: "tp-tutor.firebaseapp.com",
+  projectId: "tp-tutor",
+  storageBucket: "tp-tutor.appspot.com",
+  messagingSenderId: "784915926349", // Pulled from your Project Number
+  appId: "1:784915926349:web:86aa45878b95b170217b04" // You must paste your real App ID here
 };
 
 const app = initializeApp(firebaseConfig);
