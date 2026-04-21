6d6ff97c1e3e42df90a02f2208404a3bf3ca3733
AnthonyontheHub <Anthony.Guera@gmail.com>
2026-04-20 20:35:51 -0500

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCK-D_zNi8LazyaEhP9VLUYWeeGa2QXcI",
-  authDomain: "tp-tutor.firebaseapp.com",
-  projectId: "tp-tutor",
-  storageBucket: "tp-tutor.firebasestorage.app",
-  messagingSenderId: "784915926349",
-  appId: "1:784915926349:web:86aa45878b95b170217b04",
-  measurementId: "G-04EFDCZM9G"
+  apiKey: "...", 
+  authDomain: "tp-tutor-26792.firebaseapp.com", // Double check this
+  projectId: "tp-tutor-26792",                 // MUST match exactly
+  storageBucket: "tp-tutor-26792.appspot.com",
+  // ...
 };
 
 // 1. Initialize the app
